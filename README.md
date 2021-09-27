3e0f3f3541d7a1fdb04084a4cf8e2600944e4809
Kelvin Ducray <kelvinducray@192-168-1-101.tpgi.com.au>
2021-09-27 22:38:53 +1000

    Built out more of a skeleton - needs a lot of debugging

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# fastapi-orders-api
+# FastAPI Orders API
+
+An application to keep track of incoming orders.
+
+## Skills to demonstrate:
+- Python:
+  - OOP
+  - Async
+  - SQL Alchemy ORM
+  - Language best practices
+    - Unit Testing
+    - Type checking
+    - Linting
+- Docker (and Docker Compose)
+- Database usage (PostgreSQL)
+- CI/CD
+  - Github + Heroku
+
+## To-dos:
+- Complete app
+- Get CI/CD up and running
+- Add build passing badge
+- Unit testing
+- Add to and expand list of skills demonstrated
+
+## Potential to-dos:
+- Some slight project restructuring
+- .dockerignore
+- Different dev & prod deployments
+- Multistage Docker build
--- orders_api/app/database.py
+++ orders_api/app/database.py
@@ -1,17 +1,35 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import asyncio
 
-from config import get_settings
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.future import select
+from sqlalchemy.orm import declarative_base, relationship, selectinload, sessionmaker
+
+from .config import get_settings
 
 settings = get_settings()
 
-engine = create_engine(settings.POSTGRES_DSN)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
+engine = create_async_engine(
+    settings.POSTGRES_DSN,
+    echo=True,
 )
 
 Base = declarative_base()
+
+Session = sessionmaker(
+    bind=None,
+    class_=AsyncSession,
+    autoflush=True,
+    autocommit=False,
+    expire_on_commit=False,  # True by default
+    info=None,
+)
+
+
+def get_db():
+    try:
+        sess = Session()
+        yield sess
+    finally:
+        sess.close()
